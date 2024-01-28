bba78c40d018c8af767db89b2b0bfb0d25117dbe
Student <average.runner216@gmail.com>
2024-01-28 11:50:07 +0530

    Working on the member_details api route.

--- api/member_details.js
+++ api/member_details.js
@@ -1,6 +1,5 @@
+import fetch from "node-fetch";
 const API_KEY = "YOUR_API_KEY";
-
-let memberDetails = [];
 
 function checkApiKey(req) {
   const authHeader = req.headers.authorization;
@@ -14,22 +13,27 @@ function checkApiKey(req) {
   return false;
 }
 
-export default (req, res) => {
+async function getMembersDataFromGoogleAppsScript() {
+  const response = await fetch(
+    "https://script.google.com/macros/s/AKfycbyDOAKYy5q1PdW44wp-hXy-4mzwwHJSGZPy_8OfUMmYxXR5toLkix20vV0U0JAPhqie/exec",
+    {
+      headers: { Authorization: "Bearer YOUR_API_KEY" },
+    }
+  );
+  return response.json();
+}
+
+export default async (req, res) => {
   if (!checkApiKey(req)) {
     return res.status(401).json({ error: "Unauthorized" });
   }
 
-  if (req.method === "POST") {
+  if (req.method === "GET") {
     try {
-      const members = req.body.members;
-      memberDetails = members;
-      return res
-        .status(200)
-        .json({ message: "Member details updated successfully" });
+      const memberDetails = await getMembersDataFromGoogleAppsScript();
+      return res.status(200).json(memberDetails);
     } catch (error) {
       return res.status(500).json({ error: error.message });
     }
-  } else if (req.method === "GET") {
-    return res.status(200).json(memberDetails);
   }
 };
