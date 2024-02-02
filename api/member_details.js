4e0ffe1c4c12b3f8a2863a6bbc1d1bc3a8fe71dc
Student <average.runner216@gmail.com>
2024-02-02 10:13:03 +0530

    rollback this commit.

--- api/member_details.js
+++ api/member_details.js
@@ -3,8 +3,9 @@ import axios from "axios";
 async function getMembersDataFromGoogleAppsScript() {
   try {
     const response = await axios.get(
-      "https://script.google.com/macros/s/AKfycbwsIYmcpnpEYIS-8rHNrNxsneZCm43oUjUOIggp1P0/dev"
+      "https://script.googleusercontent.com/macros/echo?user_content_key=A-S417Y8zCQafn42q2SF8pm8yPADqWatqC583Lm-FS4y3_m57jjcda3z-HBvPqVOD83gpvs9H8nwakLrEtbk3FDh7ca-tswGm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnASciXxnS3nx4JGKSZJ7MhPwoC0sGDynUR3LJMG5A3qzBAnYQXmYTsGbbT05nNJzOHu-YW6DzHJw&lib=MN4McHg5VF7hVJ1wlZo-Il2s80ntowcNA"
     );
+    console.log(response.data);
     return response.data;
   } catch (error) {
     console.error("Error fetching data from Google Apps Script:", error);
@@ -12,5 +13,6 @@ async function getMembersDataFromGoogleAppsScript() {
   }
 }
 
-const response = getMembersDataFromGoogleAppsScript();
-console.log(response);
+getMembersDataFromGoogleAppsScript()
+  .then((data) => console.log(data))
+  .catch((error) => console.error(error));
