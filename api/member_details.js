3c52c9b0b1738c666bacb035f5918736aca85201
Student <average.runner216@gmail.com>
2024-02-08 00:30:06 +0530

    rollback this commit.

--- api/member_details.js
+++ api/member_details.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 
 export default async function handler(req, res) {
-  const url =
-    "https://script.googleusercontent.com/macros/echo?user_content_key=GQBGRfYGJ3zwbfkFN6ws1h9myh309eRESAsqNzT8L-5UqCgGLhrm7-AnvWSY5mpIGV000PmMIZnDUzGJy66RaEafFjlRJOmCm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnIURuQLlkRa5YohIF8FebwdrsRnSdNrTq4RgqB3Xi1NFuQm6K6Bjh1FFghBl97br2w&lib=MN4McHg5VF7hVJ1wlZo-Il2s80ntowcNA";
+  const apiKey = "SHOREGUARDIANS_SECRET_KEY";
+  const url = `https://script.google.com/macros/s/AKfycbwWkoGZ_kdsPXfiiIuiQWyClF6hJBnQa21R0PV_hUWXFuLEcH34FexfvfWXByPEXxwr/exec?apiKey=${apiKey}`;
+
   try {
     const response = await axios.get(url);
-    if (response.headers["content-type"].includes("application/json")) {
+    if (
+      response.headers["content-type"] &&
+      response.headers["content-type"].includes("application/json")
+    ) {
       res.status(200).json(response.data);
     } else {
       console.error("Received non-JSON response");
