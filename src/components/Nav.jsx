39ba37026885a9f846240c60a6f4c401ba51e27b
Student <average.runner216@gmail.com>
2024-02-09 18:53:02 +0530

    Added signup-form text in the nav next to the logo.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,22 +1,48 @@
+import { useState } from "react";
 import { HStack } from "@chakra-ui/react";
 import { EmailIcon } from "@chakra-ui/icons";
 
 export default function Nav() {
   const mailtoURL = `mailto:shoreguardians.chennai@gmail.com?cc=average.runner216@gmail.com&subject=Hey ShoreGuardians! I Have A Question...`;
+  const [isHovered, setIsHovered] = useState(false);
+
+  const formsText = "Signup-Form";
+  const letters = formsText.split("").map((letter, index) => (
+    <span
+      key={index}
+      className="letter"
+      style={{
+        transition: "transform 0.2s ease",
+        transform:
+          isHovered && index !== 0 ? `translateX(${index * 0.5}rem)` : "none",
+      }}
+    >
+      {letter}
+    </span>
+  ));
 
   return (
     <nav>
-      <HStack className="nav-hstack" spacing={300}>
+      <HStack className="nav-hstack" spacing={"17.5%"}>
         <a
           href="https://form.jotform.com/232942341832050"
           target="blank"
-          className="nav-image-container"
+          className="nav-item-container"
         >
           <img
             src="/shoreguardians-logo-hands.jpg"
             className="nav-image"
             alt="Part of the ShoreGuardians Logo"
           />
+          <div className="nav-text-icon-container">
+            <div
+              className="nav-links forms-text no-scale"
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+            >
+              {letters}
+            </div>
+          </div>
         </a>
         <a href="/" className="nav-links first">
           Home
