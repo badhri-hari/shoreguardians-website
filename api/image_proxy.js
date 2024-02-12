d85dd102a5acfc97034911b282070c8eba1283e4
Student <average.runner216@gmail.com>
2024-02-12 12:46:59 +0530

    Reworked the signup form.22

--- api/image_proxy.js
+++ api/image_proxy.js
@@ -1,10 +1,20 @@
 import axios from "axios";
 
 export default async function handler(req, res) {
-  const { url } = req.query;
+  let { url } = req.query;
   if (!url) {
     return res.status(400).json({ error: "No URL provided" });
   }
+
+  if (url.includes("drive.google.com")) {
+    const fileIdMatch = url.match(/id=([^&]+)/);
+    if (fileIdMatch && fileIdMatch[1]) {
+      url = `https://drive.googleusercontent.com/uc?id=${fileIdMatch[1]}&export=download`;
+    } else {
+      return res.status(400).json({ error: "Invalid Google Drive URL" });
+    }
+  }
+
   try {
     const response = await axios.get(url, {
       responseType: "arraybuffer",
