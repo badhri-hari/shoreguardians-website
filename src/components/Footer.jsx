2367b46226d409d32ddfbe4efef9b8c93cd4abb5
badhri-hari <average.runner216@gmail.com>
2024-04-25 07:55:49 +0530

    Fixing issue with Footer logo not appearing due to file path issues.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
       <Center className="footer-container">
         <footer>
           <HStack padding={"10px"}>
-            <img src="../../public/shoreguardians-logo.png" className="footer-logo"></img>
+            <img src="/shoreguardians-logo.png" className="footer-logo"></img>
             <div className="footer-text-container">
               Web Design Team: <a href="https://github.com/badhri-hari/" target="blank">Badhri Hari</a>, John Doe
             </div>
