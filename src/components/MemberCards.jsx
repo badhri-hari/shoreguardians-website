eeebf4bceee34962d462ce5aa4892543a6bdc5c3
badhri-hari <average.runner216@gmail.com>
2024-04-25 11:40:38 +0530

    MemberCards now shown in a 3 column grid.

--- src/components/MemberCards.jsx
+++ src/components/MemberCards.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { EmailIcon } from "@chakra-ui/icons";
-import { Avatar } from "@chakra-ui/react";
+import { Grid, Box, Avatar } from "@chakra-ui/react";
 
 export default function MemberCards() {
   const [members, setMembers] = useState([]);
@@ -12,35 +12,37 @@ export default function MemberCards() {
   }, []);
 
   return (
-    <div className="members-container">
-      {members.length > 0 ? (
-        members.map((member) => (
-          <div key={member.SubmissionTime} className="members-card">
-            <div className="members-card-container">
-              <h1>{member.Name}</h1>
-              <div className="members-card-contacts">
-                <a
-                  href={`mailto:${member.Email}?subject=Hey ${member.Name}!`}
-                  rel="noreferrer"
-                  target="_blank"
-                >
-                  <EmailIcon style={{ color: "black", fontSize: "20px" }} />
-                </a>
-              </div>
-              <div className="members-card-picture">
-                <Avatar
-                  size="sm"
-                  name={member.Name}
-                  src='URL(${member.Picture})'
-                  alt={`${member.Name}'s profile picture`}
-                />
+    <Grid templateColumns="repeat(3, 1fr)" className="members-container-grid">
+      <Box className="members-container">
+        {members.length > 0 ? (
+          members.map((member) => (
+            <div key={member.SubmissionTime} className="members-card">
+              <div className="members-card-container">
+                <h1>{member.Name}</h1>
+                <div className="members-card-contacts">
+                  <a
+                    href={`mailto:${member.Email}?subject=Hey ${member.Name}!`}
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    <EmailIcon style={{ color: "black", fontSize: "20px" }} />
+                  </a>
+                </div>
+                <div className="members-card-picture">
+                  <Avatar
+                    size="sm"
+                    name={member.Name}
+                    src="URL(${member.Picture})"
+                    alt={`${member.Name}'s profile picture`}
+                  />
+                </div>
               </div>
             </div>
-          </div>
-        ))
-      ) : (
-        <div className="members-card">Please wait...</div>
-      )}
-    </div>
+          ))
+        ) : (
+          <div className="members-card">Please wait...</div>
+        )}
+      </Box>
+    </Grid>
   );
 }
