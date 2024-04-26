f3e563ec04451c00b77e33469736553ccd7c0f11
badhri-hari <average.runner216@gmail.com>
2024-04-26 10:41:32 +0530

    Added glow cursor background. Added glassmorphism effect for MembersCard.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,54 @@
+import { useState, useEffect } from "react";
 import Footer from "../components/Footer";
 import Nav from "../components/Nav";
 
 export default function Home() {
+  const [position, setPosition] = useState({ left: 0, top: 0 });
+
+  const handlePointerMove = (event) => {
+    const xPosition = Math.min(event.clientX, window.innerWidth - 300);
+    const yPosition = Math.min(event.clientY, window.innerHeight - 300);
+    setPosition({
+      left: xPosition,
+      top: yPosition,
+    });
+  };
+
+  useEffect(() => {
+    const updatePosition = (event) => {
+      handlePointerMove(event);
+    };
+    window.addEventListener("mousemove", updatePosition);
+
+    return () => {
+      window.removeEventListener("mousemove", updatePosition);
+    };
+  }, []);
+
   return (
     <>
-      <Nav />
-      <div className="home-page-image-container">
-        <img
-          src="/test-image.png"
-          className="home-page-image"
-          alt="test image"
-        ></img>
+      <div
+        id="blob"
+        style={{
+          position: "absolute",
+          left: `${position.left}px`,
+          top: `${position.top}px`,
+          transition: "left 1s, top 1s",
+        }}
+      ></div>
+      <div onPointerMove={handlePointerMove}>
+        <div className="content-blur">
+          <Nav />
+          <div className="home-page-image-container">
+            <img
+              src="/test-image.png"
+              className="home-page-image"
+              alt="test image"
+            />
+          </div>
+          <Footer />
+        </div>
       </div>
-      <Footer />
     </>
   );
 }
--- src/pages/Members.jsx
+++ src/pages/Members.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { EmailIcon } from "@chakra-ui/icons";
 import { Avatar, Divider, VStack, Box, AbsoluteCenter } from "@chakra-ui/react";
 import Nav from "../components/Nav";
@@ -5,56 +6,85 @@ import MemberCards from "../components/MemberCards";
 import Footer from "../components/Footer";
 
 export default function Members() {
-  const mailtoURL0 = `mailto:average.runner216@gmail.com&subject=Hey Badhri!`;
-  const mailtoURL1 = `mailto:rishirajanmenon@gmail.com&subject=Hey Rishi!`;
+  const [position, setPosition] = useState({ left: 0, top: 0 });
+
+  const handlePointerMove = (event) => {
+    setPosition({
+      left: event.clientX,
+      top: event.clientY,
+    });
+  };
+
+  const mailtoURL0 = `mailto:average.runner216@gmail.com?subject=Hey Badhri!`;
+  const mailtoURL1 = `mailto:rishirajanmenon@gmail.com?subject=Hey Rishi!`;
 
   return (
     <>
-      <Nav page="members" />
-      <div className="members-founders-container">
-        <div className="members-founders-card">
-          <div className="members-founders-card-container">
-            <h1>Badhri N Hari</h1>
-            <p>Director</p>
-            <div className="members-founders-card-contacts">
-              <a href={mailtoURL0} target="blank">
-                <EmailIcon style={{ color: "black", fontSize: "40px" }} />
-              </a>
+      <div
+        id="blob"
+        style={{
+          position: "absolute",
+          left: `${position.left}px`,
+          top: `${position.top}px`,
+          transition: "left 0.4s, top 0.4s",
+        }}
+      ></div>
+      <div
+        style={{
+          width: "100%",
+          height: "100vh",
+          paddingTop: "10px",
+        }}
+        onPointerMove={handlePointerMove}
+      >
+        <div className="content-blur">
+          <Nav page="members" />
+          <div className="members-founders-container">
+            <div className="members-founders-card">
+              <div className="members-founders-card-container">
+                <div className="members-founders-card-text-container">
+                  <h1>Badhri N Hari</h1>
+                  <p>Director</p>
+                  <div className="members-founders-card-contacts">
+                    <a href={mailtoURL0} target="_blank" rel="noreferrer">
+                      <EmailIcon style={{ color: "black", fontSize: "40px" }} />
+                    </a>
+                  </div>
+                </div>
+                <div className="members-founders-card-picture">
+                  <Avatar name="Badhri Hari" src="/badhri-image.jpeg" />
+                </div>
+              </div>
             </div>
-            <div className="members-founders-card-picture">
-              <Avatar size={null} name="Badhri Hari" src="/badhri-image.jpeg" />
+            <div className="members-founders-card">
+              <div className="members-founders-card-container">
+                <div className="members-founders-card-text-container">
+                  <h1>Rishi Rajan Menon</h1>
+                  <p>President</p>
+                  <div className="members-founders-card-contacts">
+                    <a href={mailtoURL1} target="_blank" rel="noreferrer">
+                      <EmailIcon style={{ color: "black", fontSize: "40px" }} />
+                    </a>
+                  </div>
+                </div>
+                <div className="members-founders-card-picture">
+                  <Avatar name="Rishi Rajan Menon" src="/rishi-image.jpg" />
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="members-founders-card">
-          <div className="members-founders-card-container">
-            <h1>Rishi Rajan Menon</h1>
-            <p>President</p>
-            <div className="members-founders-card-contacts">
-              <a href={mailtoURL1} target="blank">
-                <EmailIcon style={{ color: "black", fontSize: "40px" }} />
-              </a>
-            </div>
-            <div className="members-founders-card-picture">
-              <Avatar
-                size={null}
-                name="Rishi Rajan Menon"
-                src="/rishi-image.jpg"
-              />
-            </div>
-          </div>
+          <Box position="relative" padding="30">
+            <Divider />
+            <AbsoluteCenter px="4" className="members-header">
+              Our Members
+            </AbsoluteCenter>
+          </Box>
+          <VStack>
+            <MemberCards />
+          </VStack>
+          <Footer />
         </div>
       </div>
-      <Box position="relative" padding="30">
-        <Divider />
-        <AbsoluteCenter px="4" className="members-header">
-          Our Members
-        </AbsoluteCenter>
-      </Box>
-      <VStack>
-        <MemberCards />
-      </VStack>
-        <Footer />
     </>
   );
 }
