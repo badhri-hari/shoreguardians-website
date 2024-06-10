8db20f142231618d7af6f26a0f9ec09cd2660466
badhri-hari <average.runner216@gmail.com>
2024-06-10 13:30:01 +0530

    Made Nav and Footer components responsive.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,34 @@
-import { HStack, Center } from "@chakra-ui/react";
+import { HStack, Center, VStack } from "@chakra-ui/react";
+import { useMediaQuery } from "@chakra-ui/react";
 
 export default function Footer() {
+  const [isSmallerScreen] = useMediaQuery("(max-width: 425px)");
+
   return (
-      <Center className="footer-container">
-        <footer>
+    <Center className="footer-container">
+      <footer>
+        {isSmallerScreen ? (
+          <VStack className="glassmorphism-footer" padding={"10px"}>
+            <img src="/shoreguardians-logo.png" className="footer-logo"></img>
+            <div className="footer-text-container">
+              Web Design Team:{" "}
+              <a href="https://github.com/badhri-hari/" target="blank">
+                Badhri Hari
+              </a>
+            </div>
+          </VStack>
+        ) : (
           <HStack className="glassmorphism-footer" padding={"10px"}>
             <img src="/shoreguardians-logo.png" className="footer-logo"></img>
             <div className="footer-text-container">
-              Web Design Team: <a href="https://github.com/badhri-hari/" target="blank">Badhri Hari</a>
+              Web Design Team:{" "}
+              <a href="https://github.com/badhri-hari/" target="blank">
+                Badhri Hari
+              </a>
             </div>
           </HStack>
-        </footer>
-      </Center>
+        )}
+      </footer>
+    </Center>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,58 +1,155 @@
-import { Box, HStack, Center, Link } from "@chakra-ui/react";
-import { AtSignIcon } from "@chakra-ui/icons";
+import { useState } from "react";
+import {
+  Box,
+  HStack,
+  Center,
+  Link,
+  VStack,
+  Drawer,
+  DrawerBody,
+  DrawerOverlay,
+  DrawerContent,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
 import { FaInstagram, FaUserPlus } from "react-icons/fa";
+import { IoAtSharp, IoCloseOutline } from "react-icons/io5";
 
 export default function Nav() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [show, setShow] = useState(false);
+
+  const handleToggle = () => setShow(!show);
+
   return (
-    <header className="header-container">
-      <Box className="nav-logo-container nav-logo-blue-background">
-        <img
-          src="/shoreguardians-logo-hands.jpg"
-          className="nav-image"
-          alt="ShoreGuardians Logo"
-        />
-      </Box>
-      <nav>
-        <Center className="glassmorphism-nav">
-          <HStack className="nav-hstack" spacing="2rem" width="100%">
-            <Link href="/" className="nav-links">
-              Home
-            </Link>
-            <Link href="/members" className="nav-links">
-              Members
-            </Link>
-            <Link href="/recognition" className="nav-links">
-              Recognition
-            </Link>
-            <HStack spacing="1rem">
-              <a
-                href="mailto:shoreguardians.chennai@gmail.com?cc=badhrihari123@gmail.com,rishirajanmenon@gmail.com&subject=Hey ShoreGuardians! I Have A Question..."
-                target="blank"
-              >
-                <AtSignIcon
-                  color="white"
-                  boxSize="2em"
-                  className="nav-email-container"
-                />
-              </a>
-              <a href="https://instagram.com/shore.guardians" target="blank">
-                <FaInstagram
-                  color="white"
-                  size="2em"
-                  className="nav-instagram-container"
-                />
-              </a>
-              <a href="https://forms.gle/BxeDLuxhkKXpJffi6" target="blank">
-                <FaUserPlus
-                  color="white"
-                  size="2em"
-                  className="nav-signup-container"
-                />
-              </a>
+    <>
+      <header className="header-container">
+        <Box className="nav-logo-container nav-logo-blue-background">
+          <img
+            src="/shoreguardians-logo-hands.jpg"
+            className="nav-image"
+            alt="ShoreGuardians Logo"
+          />
+        </Box>
+        <nav>
+          <Center onClick={onOpen} className="glassmorphism-nav">
+            <Box onClick={handleToggle} className="burger-menu">
+              <Center>
+                <HamburgerIcon boxSize={30} mt={10} aria-label="Open Menu" />
+              </Center>
+            </Box>
+            <HStack className="nav-hstack" spacing="2rem" width="100%">
+              <Link href="/" className="nav-links-desktop">
+                Home
+              </Link>
+              <Link href="/members" className="nav-links-desktop">
+                Members
+              </Link>
+              <Link href="/recognition" className="nav-links-desktop">
+                Recognition
+              </Link>
+              <HStack spacing="1rem" className="nav-icons-container-desktop">
+                <a
+                  href="mailto:shoreguardians.chennai@gmail.com?cc=badhrihari123@gmail.com,rishirajanmenon@gmail.com&subject=Hey ShoreGuardians! I Have A Question..."
+                  target="blank"
+                >
+                  <IoAtSharp
+                    color="white"
+                    size="2em"
+                    className="nav-email-container-desktop"
+                  />
+                </a>
+                <a href="https://instagram.com/shore.guardians" target="blank">
+                  <FaInstagram
+                    color="white"
+                    size="2em"
+                    className="nav-instagram-container-desktop"
+                  />
+                </a>
+                <a href="https://forms.gle/BxeDLuxhkKXpJffi6" target="blank">
+                  <FaUserPlus
+                    color="white"
+                    size="2em"
+                    className="nav-signup-container-desktop"
+                  />
+                </a>
+              </HStack>
             </HStack>
-          </HStack>
-        </Center>
-      </nav>
-    </header>
+          </Center>
+        </nav>
+
+        <Drawer isOpen={isOpen} onClose={onClose}>
+          <DrawerOverlay />
+          <DrawerContent
+            bg="rgba(138, 188, 215, 0.95)"
+            style={{
+              alignItems: "center",
+              justifyItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <DrawerBody>
+              <VStack spacing="2rem">
+                <Link href="/" className="nav-links-mobile" onClick={onClose}>
+                  Home
+                </Link>
+                <Link
+                  href="/members"
+                  className="nav-links-mobile"
+                  onClick={onClose}
+                >
+                  Members
+                </Link>
+                <Link
+                  href="/recognition"
+                  className="nav-links-mobile"
+                  onClick={onClose}
+                >
+                  Recognition
+                </Link>
+                <HStack spacing="1rem" className="nav-icons-container-mobile">
+                  <a
+                    href="mailto:shoreguardians.chennai@gmail.com?cc=badhrihari123@gmail.com,rishirajanmenon@gmail.com&subject=Hey ShoreGuardians! I Have A Question..."
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <IoAtSharp
+                      color="white"
+                      size="2em"
+                      className="nav-email-container-mobile"
+                    />
+                  </a>
+                  <a
+                    href="https://instagram.com/shore.guardians"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <FaInstagram
+                      color="white"
+                      size="2em"
+                      className="nav-instagram-container-mobile"
+                    />
+                  </a>
+                  <a
+                    href="https://forms.gle/BxeDLuxhkKXpJffi6"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <FaUserPlus
+                      color="white"
+                      size="2em"
+                      className="nav-signup-container-mobile"
+                    />
+                  </a>
+                </HStack>
+              </VStack>
+            </DrawerBody>
+            <Center mt={40}>
+              <IoCloseOutline onClick={onClose} size="3em" />
+            </Center>
+          </DrawerContent>
+        </Drawer>
+      </header>
+    </>
   );
 }
