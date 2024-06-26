8f5bd7aeeeb02598022fef60906701b4dc4ccd93
badhri-hari <average.runner216@gmail.com>
2024-06-26 13:13:45 +0530

    Added Vercel Speed Insights.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 import "./App.css";
 import Home from "./pages/Home.jsx";
