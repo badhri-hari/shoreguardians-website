ec29972cd651efdbba9135f9266f0119b9c44d5d
badhri-hari <average.runner216@gmail.com>
2024-06-26 12:01:17 +0530

    Update member_details api route.

--- api/member_details.js
+++ api/member_details.js
@@ -9,15 +9,19 @@ export default async function handler(req, res) {
 
   try {
     const response = await axios.get(url);
-    res.status(200).json(response);
+    const data = response.data;
+
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET");
     res.setHeader(
       "Access-Control-Allow-Headers",
       "Content-Type, Authorization"
     );
+
+    res.status(200).json(data);
   } catch (error) {
-    console.error("Error fetching data:", error);
+    console.error("Error fetching data:", error.message);
+
     res.status(500).json({ error: "Error fetching member details" });
   }
 }
