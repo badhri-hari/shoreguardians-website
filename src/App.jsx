1ea25ca04f4d36113c64fc95e3d69a71b71e1fb7
badhri-hari <average.runner216@gmail.com>
2024-07-13 14:20:56 +0530

    Moved Analytics code from App.jsx to index.html

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
-import { Analytics } from "@vercel/analytics/react";
 
 import "./App.css";
 import Home from "./pages/Home.jsx";
@@ -13,15 +12,13 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <HelmetProvider>
-      <Analytics>
-        <Router>
-          <Routes>
-            <Route path="*" element={<Home />} />
-            <Route path="/members" element={<Members />} />
-            <Route path="/recognition" element={<Recognition />} />
-          </Routes>
-        </Router>
-      </Analytics>
+      <Router>
+        <Routes>
+          <Route path="*" element={<Home />} />
+          <Route path="/members" element={<Members />} />
+          <Route path="/recognition" element={<Recognition />} />
+        </Routes>
+      </Router>
     </HelmetProvider>
   </React.StrictMode>
 );
